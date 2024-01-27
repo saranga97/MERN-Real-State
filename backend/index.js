f9e2777016761e6899af78f150de51a1e4b2c169
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-01-27 20:20:30 +0530

    Initiated database connectivity

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
-
