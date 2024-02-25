19fbec46364d209088daa2e5d6c777d1ce88dd00
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-02-25 16:10:04 +0530

    create update user api route

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,3 +1,37 @@
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
 export const test = (req, res) => {
   res.json({ message: "Hello world" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(403, "You can update only your own account"));
+
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 12);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
