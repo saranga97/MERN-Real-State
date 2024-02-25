f828b43ef2d5141db76142d3f1e0e06d048c855a
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-02-25 22:30:55 +0530

    Delete user functionality

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -35,3 +35,16 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+
+  if(req.user.id !== req.params.id) return next(errorHandler(403, "You can delete only your own account"));
+
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({message: "User has been deleted"}).clearCookie("access_token");
+    
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,7 +9,7 @@ import {
 } from "firebase/storage";
 import { app } from "../firebase";
 import { set } from "mongoose";
-import {updateUserStart, updateUserSuccess, updateUserFailure} from '../redux/user/userSlice.js'
+import {updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess} from '../redux/user/userSlice.js'
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -79,6 +79,25 @@ console.log(formData);
       dispatch(updateUserFailure(error.message));
     }
   };
+
+  const handleDeleteUser = async () => {
+    
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/backend/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+
+  };
   return (
     <div className=" p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -137,7 +156,7 @@ console.log(formData);
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className=" text-red-700 cursor-pointer">Delete account</span>
+        <span onClick={handleDeleteUser} className=" text-red-700 cursor-pointer">Delete account</span>
         <span className=" text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className=" text-red-700 mt-5">
